d777f3dc340935f991fad0e63de5bb47f8732a47
Chen Yue <yuechen049@gmail.com>
2024-09-18 22:21:38 -0400

    implemented search bar in inbox page

--- RealEstateApp/app/(tabs)/(inbox)/inbox.js
+++ RealEstateApp/app/(tabs)/(inbox)/inbox.js
@@ -1,33 +1,55 @@
-import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, SafeAreaView, View, TextInput, FlatList } from 'react-native';
 import { router } from 'expo-router'
 import { Colors } from '../../../constants/Colors'
 import MessageCard from '../../../components/inbox/MessageCard'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Inbox() {
+    // temporary data
     const tempData = [
         {
             id: '1',
-            username: 'Landlord 1',
+            username: 'Alice',
             previewMessage: 'Hello, I am interested in your property!',
-            latestMessageTime: '12:00pm'
+            latestMessageTime: '12:05'
         },
         {
             id: '2',
-            username: 'Landlord 2',
+            username: 'Bob',
             previewMessage: 'Hello! I saw your posting about a property in Montreal. I am interested in your property!',
-            latestMessageTime: '11:00pm'
+            latestMessageTime: '13:21'
         }
     ]
 
+    // search box query
+    const [query, setQuery] = useState('')
+
+    // navigate to single message chat page
     const navigateToMessage = (id) => {
         router.push(`./message/${id}`)
     }
 
     return (
         <SafeAreaView style={styles.container}>
+            <View style={styles.searchBoxContainer}>
+                <FontAwesome name="search" size={16} color={Colors.appGray} />
+                <TextInput
+                    autoCapitalize='none'
+                    style={styles.searchBox}
+                    onChangeText={setQuery}
+                    value={query}
+                    placeholder='Search users'
+                />
+            </View>
+
             <FlatList
                 data={tempData}
-                renderItem={({ item }) => <MessageCard inboxItem={item} onPress={() => navigateToMessage(item.id)} />}
+                renderItem={({ item }) => {
+                    if (item.username.toLowerCase().includes(query.toLowerCase())) {
+                        return <MessageCard inboxItem={item} onPress={() => navigateToMessage(item.id)} />
+                    }
+                }}
                 keyExtractor={item => item.id}
 
             />
@@ -39,5 +61,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: Colors.appLight
+    },
+    searchBoxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        height: 40,
+        margin: 10,
+        paddingHorizontal: 15,
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor: Colors.appGrayDark
+    },
+    searchBox: {
+        flex: 1
     }
 });
