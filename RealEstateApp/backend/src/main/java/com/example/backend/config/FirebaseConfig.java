7b1c630502119f3c55dff9eb2f178726163feb0c
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-09-23 03:56:01 -0400

    FireStore configuration

--- RealEstateApp/backend/src/main/java/com/example/backend/config/FirebaseConfig.java
+++ RealEstateApp/backend/src/main/java/com/example/backend/config/FirebaseConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 @Configuration
 public class FirebaseConfig {
@@ -22,6 +24,7 @@ public class FirebaseConfig {
             @SuppressWarnings("deprecation")
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://realestate-64b3d-default-rtdb.firebaseio.com")
                 .build();
 
             // Initialize Firebase App with a custom name
@@ -32,6 +35,11 @@ public class FirebaseConfig {
             
             return app;
         
+    }
+
+    @Bean
+    public Firestore firestore() {
+        return FirestoreClient.getFirestore();
     }
 
 }
