617a9613a589cef2acc559a31cba374e336d6811
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-09-23 03:56:35 -0400

    save on firestore

--- RealEstateApp/backend/src/main/java/com/example/backend/entity/User.java
+++ RealEstateApp/backend/src/main/java/com/example/backend/entity/User.java
@@ -4,6 +4,11 @@ public class User {
     private String email;
     private String password;
 
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
     // Getters and Setters
     public String getEmail() {
         return email;
