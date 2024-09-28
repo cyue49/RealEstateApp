99513cc0111d885ccc057014583a4539ac1483d1
Chen Yue <yuechen049@gmail.com>
2024-09-27 22:58:59 -0400

    updated handleDelete for deleting a chat

--- RealEstateApp/components/inbox/MessageCard.js
+++ RealEstateApp/components/inbox/MessageCard.js
@@ -14,6 +14,7 @@ export default MessageCard = ({ item, onPress }) => {
     const [deletePopupVisible, setDeletePopupVisible] = useState(false);
     const [renamePopupVisible, setRenamePopupVisible] = useState(false);
     const [input, setInput] = useState('') // input for popup modal
+    const [deleted, setDeleted] = useState(false) // whether this chat has been deleted
 
     // set date, input, chat name
     useEffect(() => {
@@ -63,7 +64,15 @@ export default MessageCard = ({ item, onPress }) => {
 
     // handle delete for delete pop up modal
     const handleDelete = () => {
-        console.log('deleted')
+        axios.delete(`${baseURL}/api/chats/id/${item.id}/delete`)
+            .then(() => {
+                setDeleted(true)
+
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+
         setDeletePopupVisible(false)
     }
 
@@ -80,33 +89,37 @@ export default MessageCard = ({ item, onPress }) => {
             .catch((e) => {
                 console.log(e)
             })
+
         setRenamePopupVisible(false)
     }
 
     return (
         <View>
-            <TouchableOpacity
-                onPress={onPress}
-                onLongPress={handleLongPress}>
-                <View style={styles.messageCard}>
-                    <View style={styles.imageContainer}>
-                        <Image
-                            style={styles.profileImage}
-                            source={require('../../assets/default-profile.png')} // setOptionsModalVisibleorary image
-                        />
-                    </View>
-
-                    <View style={{ flex: 1, flexDirection: 'column' }}>
-                        <View style={styles.titleRow}>
-                            <Text numberOfLines={1} style={styles.chatName}>{displayName}</Text>
-                            <Text style={styles.messageTime}>{displayTime}</Text>
+            {deleted ? null :
+                <TouchableOpacity
+                    onPress={onPress}
+                    onLongPress={handleLongPress}>
+                    <View style={styles.messageCard}>
+                        <View style={styles.imageContainer}>
+                            <Image
+                                style={styles.profileImage}
+                                source={require('../../assets/default-profile.png')} // setOptionsModalVisibleorary image
+                            />
                         </View>
-                        <Text numberOfLines={1} style={styles.previewMessage}>{item.latestMessage === "" ? "No messages yet" : item.latestMessage}</Text>
-                    </View>
 
-                    <FontAwesome name="angle-right" size={24} color={Colors.appBlueDark} />
-                </View>
-            </TouchableOpacity>
+                        <View style={{ flex: 1, flexDirection: 'column' }}>
+                            <View style={styles.titleRow}>
+                                <Text numberOfLines={1} style={styles.chatName}>{displayName}</Text>
+                                <Text style={styles.messageTime}>{displayTime}</Text>
+                            </View>
+                            <Text numberOfLines={1} style={styles.previewMessage}>{item.latestMessage === "" ? "No messages yet" : item.latestMessage}</Text>
+                        </View>
+
+                        <FontAwesome name="angle-right" size={24} color={Colors.appBlueDark} />
+                    </View>
+                </TouchableOpacity>
+            }
+
 
             <SlideUpModal isVisible={optionsModalVisible} setisVisible={setOptionsModalVisible} handleCancel={handleCancel} message="Please choose an option:" options={["Rename Chat", "Delete Chat"]} handleOptions={[onChooseRename, onChooseDelete]} />
 
